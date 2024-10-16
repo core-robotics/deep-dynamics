6e5dc895ed6958a5eb55846ce63bdbe2cd450b8c
DoHyun_Kwon <tlstoddk99@gmail.com>
2024-10-16 14:04:57 +0900

    add sim_data

--- deep_dynamics/tools/mcap_to_csv.py
+++ deep_dynamics/tools/mcap_to_csv.py
@@ -4,8 +4,8 @@ from mcap.reader import make_reader
 from tqdm import tqdm  
 
 # file path
-bagfile_path = "/home/a/Downloads/rosbag2_2024_10_14-14_47_22/rosbag2_2024_10_14-14_47_22_0.mcap"
-output_csv_path = "/home/a/deep-dynamics/deep_dynamics/csv/rosbag2_2024_10_14.csv"
+bagfile_path = "/home/a/bags/241016/rosbag2_2024_10_16-13_46_53/rosbag2_2024_10_16-13_46_53_0.mcap"
+output_csv_path = "/home/a/deep-dynamics/deep_dynamics/csv/rosbag2_2024_10_16_sim.csv"
 
 # message format and fieldnames
 MESSAGE_FORMAT = "15d"  # float64 , 15 data fields
@@ -14,21 +14,23 @@ fieldnames = [
     'a', 'ax', 'ay', 'slip_angle', 'accel', 'jerk', 'steer', 'steer_vel'
 ]
 
+# Define the topic name to filter
+target_topic = "/state0"  # Modify this to the desired topic name
+
 # MCAP file read and write to CSV
 with open(bagfile_path, 'rb') as f, open(output_csv_path, 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
     message_reader = make_reader(f)
-    total_messages = sum(1 for _ in message_reader.iter_messages())  # 메시지 개수 계산
-    
-    
+    total_messages = sum(1 for _ in message_reader.iter_messages())  
+
     f.seek(0) 
     message_reader = make_reader(f)
 
     for schema, channel, message in tqdm(message_reader.iter_messages(), total=total_messages, desc="Processing MCAP messages"):
-        if "CarState" in schema.name:
-            # 4byte header excluded
+        if "CarState" in schema.name and channel.topic == target_topic:
+            # 4-byte header excluded
             data = struct.unpack(MESSAGE_FORMAT, message.data[4:])
             # Write the extracted data to CSV
             writer.writerow(dict(zip(fieldnames, [message.log_time] + list(data))))
